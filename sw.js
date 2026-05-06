84fad8f9d9e1d45e4adb60d97d1b0a3f6268fe08
rabean80 <rabean80@gmail.com>
2026-05-06 23:50:41 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'ayo-baking-v1';
+const ASSETS = [
+  '/ayo-baking/',
+  '/ayo-baking/index.html',
+  '/ayo-baking/baking_scheduler_v2.html',
+  '/ayo-baking/manifest.json'
+];
+
+// 설치: 핵심 파일 캐시
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// 활성화: 오래된 캐시 삭제
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 우선, 실패 시 캐시 사용
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
